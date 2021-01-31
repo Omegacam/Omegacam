8b9dfd37e55131814d9f625d1003f10677ec94f7
richardwei6 <48197502+richardwei6@users.noreply.github.com>
2021-01-31 12:53:33 -0800

    Replace with working scripts

--- Networking Scripts/pub.py
+++ Networking Scripts/pub.py
@@ -1,36 +1,18 @@
-# Telemetry ZeroMQ Publisher
-# Copyright (c) 2020 Applied Engineering
-
-import concurrent.futures
-import logging
-import msgpack
-import queue
-import serial
-import serial.tools.list_ports
-import threading
-import traceback
 import zmq
 import time
 
-# Set logging verbosity.
-# CRITICAL will not log anything.
-# ERROR will only log exceptions.
-# INFO will log more information.
-log_level = logging.INFO
+ctx = zmq.Context()
+sock = ctx.socket(zmq.PUB)
+sock.bind("tcp://*:1234")
 
-# ZeroMQ Context.
-context = zmq.Context.instance()
-# Define the socket using the Context.
-pub = context.socket(zmq.PUB)
-pub.bind('tcp://127.0.0.1:5555')
-
-data = "test"
-
+print("Starting loop...")
+i = 1
 while True:
-    #radio.send(str.encode(data), group='t')
-    pub.send(data)
-    print('sent')
+    msg = "Hi for the %d:th time..." % i
+    sock.send_string(msg)
+    print("Sent string: %s ..." % msg)
+    i += 1
     time.sleep(1)
 
-pub.close()
-context.term()
+sock.close()
+ctx.term()
--- Networking Scripts/sub.py
+++ Networking Scripts/sub.py
@@ -1,48 +1,15 @@
-# Telemetry ZeroMQ Subscriber
-# Copyright (c) 2020 Applied Engineering
-
-import logging
-import msgpack
-import traceback
 import zmq
+import time
 
-# Set logging verbosity.
-# CRITICAL will not log anything.
-# ERROR will only log exceptions.
-# INFO will log more information.
-log_level = logging.INFO
+ctx = zmq.Context()
+sock = ctx.socket(zmq.SUB)
+sock.connect("tcp://192.168.1.10:1234")
+sock.subscribe("") # Subscribe to all topics
 
-# ZeroMQ Context.
-context = zmq.Context.instance()
-# Define the socket using the Context.
-sub = context.socket(zmq.SUB)
-# Define connection address.
-address = 'tcp://127.0.0.1:5555'
-# Define subscription.
-group = 't'
-# Establish the connection.
-sub.connect(address)
-#dish.join(group)
-# Set receive timeout.
-#dish.rcvtimeo = 1000
+print("Starting receiver loop ...")
+while True:
+    msg = sock.recv_string()
+    print("Received string: %s ..." % msg)
 
-sub.setsockopt(zmq.SUBSCRIBE, b't')
-
-if __name__ == '__main__':
-    try:
-        logging.basicConfig(format='[%(asctime)s] %(levelname)s: %(message)s', level=log_level, datefmt="%H:%M:%S")
-        logging.info('Listening for "%s" group data from %s.', group, address)
-        while True:
-            try:
-                print(sub.recv(copy=False, flags=zmq.NOBLOCK))
-                logging.info('Received data.')
-            except zmq.ZMQError as e:
-                if e.errno == zmq.EAGAIN:
-                    pass    # no message ready yet
-                else:
-                    traceback.print_exc()
-    except KeyboardInterrupt:
-        logging.info('Exiting now.')
-        sub.close()
-    except:
-        traceback.print_exc()
+sock.close()
+ctx.term()
